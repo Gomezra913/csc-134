4e42fef461a6ea85930b05cd8903cb5d97ec33f5
Gomezra913 <gomezrar4456@student.faytechcc.edu>
2026-08-19 11:10:23 -0400

    Uploaded M1T1

---
+++ main.cpp
@@ -0,0 +1,25 @@
+// one line comment
+/*
+block comment
+M1T1 - Hello World
+Gomez
+8/19/26
+First program - just a smoke test.
+*/
+
+// magic words to start u[
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world CSC 134!" << endl;
+    // name is a VARIABLE - a verb
+    string name;
+    cout << "What's your name? ";
+    cin >> name; // input - reads the name into this variable
+
+    cout << "This program is for : " << name << endl;
+    return 0; // no errors
+}
