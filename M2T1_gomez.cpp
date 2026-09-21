3dc1d1c069563a12f520db9504842e948e26f92e
Gomezra913 <gomezrar4456@student.faytechcc.edu>
2026-09-21 00:11:35 +0000

    Finished M1T1

--- M2T1_gomez.cpp
+++ M2T1_gomez.cpp
@@ -4,6 +4,7 @@
 // check out machine 
 
 #include <iostream>
+#include <iomanip> // For the 2 decimal places 
 using namespace std;
 
  int main() {
@@ -11,6 +12,7 @@ using namespace std;
     // set all veriables 
     string first_name, last_name, full_name ; // holds customer name 
     string product = "phone"; // changed 
+    int amount_purchased; 
     double cost_each = 0.99; 
     double total_cost; 
 
@@ -23,14 +25,20 @@ using namespace std;
     full_name = first_name + " " + last_name; 
     cout << "Nice to meet you, " << full_name << endl; 
 
+    // Ask how much they'd like to purchase
+    cout << "how many " << product << " would you like today? ";
+    cin >> amount_purchased;
 
+    //calculate total price 
+    total_cost = amount_purchased * cost_each; 
 
+    // Formating: set all prices to 2 decimal places 
+    cout << setprecision(2) << fixed;
 
-
-
-
-
-
+    // Give the result 
+    cout << "For" << amount_purchased << " " << product << endl; 
+    cout << "That will be: $" << total_cost << endl; 
+    cout << " Thank you for shopping with us!" << endl;  
 
 
 
