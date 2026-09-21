b3d9fd2deb7194f7f43028541c0479e5336fea16
Gomezra913 <gomezrar4456@student.faytechcc.edu>
2026-09-21 15:10:52 +0000

    M3T1 Finished

--- M3/M3T1_gomez.cpp
+++ M3/M3T1_gomez.cpp
@@ -16,8 +16,6 @@ int main() {
     cin >> length1; 
     cout << "\twidth  ? ";
     cin >> width1; 
-    // another way to do it:
-    // cout << "enter "
 
     cout << "2nd retangle:" << endl; 
     cout << "\tlength ? "; 
@@ -25,28 +23,28 @@ int main() {
     cout << "\twidth  ? "; 
     cin >> width2; 
 
+    // another way to do it:
+    // cout << "enter rectangle 1's diensions, put a space between the." << endl; 
+    //cin >> length1 >> width1; 
 
-
-
+    // Calculation: find area of both
     area1 = length1 * width2;
     area2 = length2 * width2;
 
-
+    // output: print the areas 
     cout << "Rectangle 1 is area: " << area1 << endl;
     cout << "rectangle 2 is area: " << area2 << endl; 
-
-
-
-
-
-
-
-
-
-
-
-
-
+    //TODO: Tell the user which one is larger (more area)
+    // if statement test for true or false 
+    if (area1 > area2) {
+        cout << "Reactangle 1 is larger." << endl;
+    }
+    else if (area2 > area1) { 
+        cout << "Rectangle 2 is larger." << endl;
+    }
+    else {
+        cout << "They are the same size." << endl;
+    }
 
 
 
